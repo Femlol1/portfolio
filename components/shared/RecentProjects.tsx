50d87d5f0674ae69d432d34d3d1be412be6fdf41
Femi <osibemekunfemi@gmail.com>
2024-11-01 12:54:15 +0000

    spell check

--- components/shared/RecentProjects.tsx
+++ components/shared/RecentProjects.tsx
@@ -7,7 +7,7 @@ const RecentProjects = () => {
 	return (
 		<div className="py-20" id="projects">
 			<h1 className="heading">
-				A small sellection of{" "}
+				A small selection of{" "}
 				<span className="text-purple"> Recent PROJECTS</span>
 			</h1>
 			<div className="flex flex-wrap items-center justify-center p-4 gap-x-24 gap-y-8 mt-10">
