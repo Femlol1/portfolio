26be5eb255cb535d2f781895ef32e83878ebdb43
Femi <osibemekunfemi@gmail.com>
2024-11-01 13:30:28 +0000

    amen

--- data/index.ts
+++ data/index.ts
@@ -69,7 +69,7 @@ export const gridItems = [
 	},
 	{
 		id: 6,
-		title: "Do you want to start a project together?",
+		title: "Does starting a project with us sound interesting?",
 		description: "",
 		className: "lg:col-span-2 md:col-span-3 md:row-span-1",
 		imgClassName: "",
